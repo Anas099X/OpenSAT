40eda65acf14fbe4bfa9bbef36cdd8575e9939e9
Anas099X <Anasshohdy400@gmail.com>
2024-08-15 12:48:02 +0300

    added vercel analyics

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Defaults = (Meta(name="viewport", content="width=device-width"),
   }
 };'''),
             Script(src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"),
+            Script(src="/_vercel/insights/script.js"),
                 Title("OpenSAT"),
                 Style(open("main.css").read())
                 
