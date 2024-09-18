dffbe9bf88174cd8b517b4d5c0afcb5134b3d5aa
Anas099X <Anasshohdy400@gmail.com>
2024-09-18 15:59:59 +0300

    adding more changes to practice

--- main.py
+++ main.py
@@ -321,7 +321,7 @@ def get(session,practice:str,module:str):
                         Div(
                         A("Back", hx_post=[f'/previous_page/{practice}/{module}' if session['page'] > 0 else None],hx_swap="innerHTML",hx_target='#practice_html',cls="btn btn-secondary", style="font-size:0.9em;"),
                         H4(session['page'] + 1),
-                        A("Next", hx_post=f'/next_page/{practice}/{module}',hx_swap="innerHTML",hx_target='#practice_html',cls="btn btn-secondary", style="font-size:0.9em;") if session['page'] == 54 else A("Finish", hx_post=f'/next_page/{practice}/{module}',hx_swap="innerHTML",hx_target='#practice_html',cls="btn btn-secondary", style="font-size:0.9em;"),
+                        A("Next", hx_post=f'/next_page/{practice}/{module}',hx_swap="innerHTML",hx_target='#practice_html',cls="btn btn-secondary", style="font-size:0.9em;") if session['page'] < 53 else A("Finish", hx_post=f'/next_page/{practice}/{module}',hx_swap="innerHTML",hx_target='#practice_html',cls="btn btn-secondary", style="font-size:0.9em;"),
                         style="display:flex; justify-content:space-between;"
                         ),
                         cls="practice-container"
